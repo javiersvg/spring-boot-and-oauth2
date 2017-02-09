d9b3a709b9d8730937f3eb7107f4bcabf4c7626b
Javier <javier.svg@gmail.com>
2017-02-09 09:38:39 -0600

    add authserver and reorganize folders to fir client application

--- ui/src/main/java/com/example/SocialApplication.java
+++ ui/src/main/java/com/example/SocialApplication.java
@@ -11,15 +11,20 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +39,8 @@ import java.util.List;
 @SpringBootApplication
 @EnableOAuth2Client
 @RestController
+@EnableAuthorizationServer
+@Order(6)
 public class SocialApplication extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -42,7 +49,7 @@ public class SocialApplication extends WebSecurityConfigurerAdapter {
     @Autowired
     CustomerRepository customerRepository;
 
-    @RequestMapping("/user")
+    @RequestMapping({ "/user", "/me" })
     public Principal user(Principal principal) {
         Customer customer = customerRepository.getByName(principal.getName());
         if (customer==null) {
@@ -74,6 +81,18 @@ public class SocialApplication extends WebSecurityConfigurerAdapter {
         return registration;
     }
 
+    @Configuration
+    @EnableResourceServer
+    protected static class ResourceServerConfiguration
+            extends ResourceServerConfigurerAdapter {
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http
+                    .antMatcher("/me")
+                    .authorizeRequests().anyRequest().authenticated();
+        }
+    }
+
 	public static void main(String[] args) {
 		SpringApplication.run(SocialApplication.class, args);
 	}
@@ -87,9 +106,11 @@ public class SocialApplication extends WebSecurityConfigurerAdapter {
                 .permitAll()
             .anyRequest()
                 .authenticated()
+                .and().exceptionHandling()
+                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"))
             .and().logout().logoutSuccessUrl("/").permitAll()
             .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-            .and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);;
+            .and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
     }
 
     private Filter ssoFilter() {
